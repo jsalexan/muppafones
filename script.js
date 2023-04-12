194eba4ac2331bf19069434498bf27f0ce5b05fd
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2023-04-12 16:52:43 -0700

    Got sound working for puffs. Enlarged mallet. Added new Ow clips for C and D puffs. Will record more tomorrow. Created Puffafones banner and styled page anew.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Function to change image 1 to image 2
 function changeImage(event) {
   let image = event.target;
@@ -7,14 +6,42 @@ function changeImage(event) {
   image.src = newSrc;
   setTimeout(function() {
     image.src = originalSrc;
-  }, 500);
+  }, 600);
+  console.log(event.target);
+}
+
+function playSound(dataPuff) {
+  const audio = document.querySelector(`audio[data-puff="${dataPuff}"]`);
+  if (audio) {
+    audio.currentTime = 0; // start from the beginning if the audio has already played
+    audio.play();
+  }
 }
 
 // Creates event listener for each puff
 let images = document.querySelectorAll('.puff1 img');
 images.forEach(function(image) {
-  image.addEventListener('mouseover', changeImage);
+  let dataPuff = image.parentNode.getAttribute('data-puff');
+  image.addEventListener('mouseenter', function(event) {
+    changeImage(event);
+    playSound(dataPuff);
+  });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
 
  
